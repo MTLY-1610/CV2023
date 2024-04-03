18906e6c6fd1b155041f46eaf04a157a5d178c13
MTLY-1610 <larrouy.m@me.com>
2024-04-03 15:19:20 +0200

    debug nav

--- nav/nav.js
+++ nav/nav.js
@@ -39,9 +39,11 @@ ScrollTrigger.create({
     markers: false
 });
 
-gsap.to(".lander", { opacity: 1,delay: 0.5,  duration: 1.5, ease: "power0.in"});
+gsap.to(".lander", { opacity: 1,delay: 0.5,  duration: 1.5, ease: "power0.inout"});
 gsap.to(".lander", { opacity: 0,  duration: 1.5, delay: 2.5, ease: "power2.out"});
-gsap.to(".menu", { opacity: 1, x: -30, duration: 1, ease: Bounce.in, delay: 5.5});
+gsap.to(".menu", { opacity: 1, x: -30, duration: .5, ease: Bounce.in, delay: 5.5});
+gsap.to(".pitch", { opacity: 1, duration: .5, delay: 5.5});
+
 
 
 // gsap.to(".pitch", { opacity: 0, duration: 0, delay: 0});
