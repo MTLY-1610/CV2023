501a9fb25ce5bfafcdcedc9b42128ab511512881
MTLY-1610 <larrouy.m@me.com>
2024-04-05 15:53:42 +0200

    pitch placeholder unworked

--- nav/nav.js
+++ nav/nav.js
@@ -37,16 +37,10 @@ ScrollTrigger.create({
     markers: false
 });
 
-gsap.to(".lander", { opacity: 1,delay: 0.5,  duration: 1.5, ease: "power0.inout"});
-gsap.to(".lander", { opacity: 0,  duration: 1.5, delay: 2.5, ease: "power2.out"});
+gsap.to(".namewrapper", { opacity: 1,delay: 0.5,  duration: 1.5, ease: "power0.inout"});
+gsap.to(".namewrapper", { toggleClass: 'disable', opacity: 0,  duration: 1.5, delay: 2.5,      ease: "power2.out"});
 gsap.to(".menu", { opacity: 1, x: -30, duration: .2, ease: Bounce.in, delay: 3.75});
 gsap.to(".pitch", { opacity: 1, duration: .5, delay: 5.5});
 
-
-
-// gsap.to(".pitch", { opacity: 0, duration: 0, delay: 0});
-// gsap.to("#namewrapper", { y: "-30vh", opacity: 1, fontSize: "2vw", ease: "power1.out", duration: .35, delay: 5 });
-// gsap.to("#burger", { opacity: 1, duration: 2, delay: 9 });
-// gsap.to("#pitch", { opacity: 1, height: "66vh", duration: 2, delay: 10 });
 
 
