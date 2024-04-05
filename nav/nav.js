73447f00e6ba544484925a3c554de96977c94fe8
MTLY-1610 <larrouy.m@me.com>
2024-04-05 19:29:10 +0200

    [...]

--- nav/nav.js
+++ nav/nav.js
@@ -37,9 +37,9 @@ ScrollTrigger.create({
     markers: false
 });
 
-gsap.to(".namewrapper", { opacity: 1,delay: 0.5,  duration: 1.5, ease: "power0.inout"});
-gsap.to(".namewrapper", { toggleClass: 'disable', opacity: 0,  duration: 1.5, delay: 2.5,      ease: "power2.out"});
-gsap.to(".menu", { opacity: 1, x: -30, duration: .2, ease: Bounce.in, delay: 3.75});
+gsap.to(".namewrapper", { opacity: 1,delay: 0.5,  duration: 2, ease: "power0.inout"});
+gsap.to(".namewrapper", { opacity: 0,  duration: 1.35, delay: 2.75, ease: "power2.out"});
+gsap.to(".menu", { opacity: 1, duration: 1.35, ease: Bounce.in, delay: 2.75});
 gsap.to(".pitch", { opacity: 1, duration: .5, delay: 5.5});
 
 
