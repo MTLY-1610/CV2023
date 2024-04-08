b9609fb08ada1e629a2fbe6388decbd045ba350d
MTLY-1610 <larrouy.m@me.com>
2024-04-08 09:07:02 +0200

    (...)

--- nav/nav.js
+++ nav/nav.js
@@ -40,7 +40,7 @@ ScrollTrigger.create({
 gsap.to(".namewrapper", { opacity: 1, delay: 0.5, duration: 2, ease: "power0.inout" });
 gsap.to(".namewrapper", { opacity: 0, duration: 1.35, delay: 2.75, ease: "power2.out" });
 gsap.to(".menu", { opacity: 1, duration: 1.35, ease: Bounce.in, delay: 2.75 });
-gsap.to(".pitch", { opacity: 1, duration: .5, delay: 5.5 });
+gsap.to(".pitch", { opacity: 1, duration: .5, delay: 2.75 });
 
 
-
+ 
