2a123e6edacce08671b7c547e7f8ad4acf8965c6
MTLY-1610 <larrouy.m@me.com>
2024-08-18 21:30:52 +0200

    menu test

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -1,19 +1,47 @@
 import React, { useRef, useEffect } from "react";
-import styles from './Menu.module.css'
+import styles from './Menu.module.css';
+
 
 function Menu() {
   return (
+
     <div className={styles.menu__wrapper}>
-      <span>
-        NAME
-      </span>
-      <span>
-       LOGO
-      </span>
-      <span>
-        BURGER
-      </span>
+      <div className={styles.menu__items}>
+        <div className={styles.menu__item__left}>
+          <span className={styles.menu__name}>
+            MATHIEU LARROUY
+          </span>
+          <span className={styles.menu__name}>
+            frontend developer
+          </span>
+          <span className={styles.menu__name}>
+            ui dedsigner
+          </span>
+        </div>
+        <div className={styles.menu__item__center}>
+          <span className={styles.menu__img}>
+            some
+          </span>
+          <span className={styles.menu__img}>
+            LOGO
+          </span>
+        </div>
+        <div className={styles.menu__item__right}>
+          <span className={styles.menu__burger}>
+          about me 
+          </span>
+          <span className={styles.menu__burger}>
+          web development
+          </span>
+          <span className={styles.menu__burger}>
+            ui design
+          </span>
+        </div>
+
+      </div>
+      <div className={styles.menu__border}></div>
     </div>
+
   );
 }
 
