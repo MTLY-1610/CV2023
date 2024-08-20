e646eb502425744b819fe02282fb172be65f35de
MTLY-1610 <larrouy.m@me.com>
2024-08-20 22:15:23 +0200

    shader test

--- src/components/floatingstuff/FloatingStuff.js
+++ src/components/floatingstuff/FloatingStuff.js
@@ -25,26 +25,40 @@ const FloatingText = () => {
     }
   });
 
+  const gradientMaterial = new THREE.ShaderMaterial({
+    vertexShader: `
+      varying vec3 vPosition;
+      void main() {
+        vPosition = position;
+        gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+      }
+    `,
+    fragmentShader: `
+      varying vec3 vPosition;
+      void main() {
+        float zMix = (vPosition.z + 2.0) / 4.0; // Normalize z position to 0-1 range
+        vec3 color1 = vec3(1.0, 0.4, 0.7); // Pink
+        vec3 color2 = vec3(0.4, 0.7, 1.0); // Light Blue
+        gl_FragColor = vec4(mix(color1, color2, zMix), 1.0);
+      }
+    `,
+    side: THREE.DoubleSide,
+  });
+
   return (
     <Text3D
       ref={textRef}
-      font="/fonts/Druk Wide Super _Italic.json" 
+      font="/fonts/Druk Wide Super _Italic.json"
       size={1}
-      height={0.2}
+      height={0.4}
       curveSegments={12}
       bevelEnabled
       bevelThickness={0.1}
-      bevelSize={.01}
+      bevelSize={0.1}
       bevelSegments={5}
+      material={gradientMaterial} // Apply the gradient material
     >
       HIRE ME
-      <meshStandardMaterial 
-      color="#ff69b4" 
-      emissive={181111}
-      metalness={0.3}  
-      roughness={0.3}  
-
-      />
     </Text3D>
   );
 };
@@ -54,7 +68,7 @@ const FloatingStuff = () => {
 
   React.useEffect(() => {
     if (cameraRef.current) {
-      cameraRef.current.position.z = 12; // Point of view
+      cameraRef.current.position.z = 15; 
     }
   }, []);
 
@@ -74,7 +88,7 @@ const FloatingStuff = () => {
       <ambientLight />
       <pointLight position={[10, 10, 10]} />
       <FloatingText />
-      <OrbitControls enableZoom={true} />
+      <OrbitControls enableZoom={false} />
     </Canvas>
   );
 };
