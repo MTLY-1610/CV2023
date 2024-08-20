65b4ad1ee73c4e96f38057b3c41ff36fe273eab5
MTLY-1610 <larrouy.m@me.com>
2024-08-20 17:45:16 +0200

    pitch text word hover effect example

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -1,11 +1,16 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { gsap } from 'gsap';
 import styles from './Hero.module.css';
+import Image from "next/image";
+import smiley from 'public/fonts/smiley.png'
+
 
 
 const Hero= () => {
   const heroGreet = useRef(null);
   const heroPitch = useRef(null);
+  const [isImageVisible, setIsImageVisible] = useState(false);
+  const textRef = useRef(null);
 
   useEffect(() => {
     const tl = gsap.timeline(); 
@@ -20,14 +25,31 @@ const Hero= () => {
     };
   }, []);
 
+  useEffect(() => {
+    if (isImageVisible) {
+      gsap.to(textRef.current, { opacity: 1, duration: 0.5});
+    } else {
+      gsap.to(textRef.current, { opacity: 0, duration: 0.5});
+    }
+  }, [isImageVisible]);
+
   return (
     <div className={styles.hero__header}>
       <div ref={heroGreet} className={styles.hero__greet}>
       <h1>hello or some greetings</h1>
       </div>
-      <div ref={heroPitch} className={styles.hero__pitch}>
-      <h1>presentation text with some fancy image display on hover</h1>
+      <div ref={heroPitch}  className={styles.hero__pitch}>
+      <h1>presentation <span onMouseEnter={() => setIsImageVisible(true)} onMouseLeave={() => setIsImageVisible(false)} className={styles.hero__hover}> text</span> with some fancy image display on hover</h1>
       </div>
+       <Image
+        ref={textRef}
+        width={300}
+        height={300}
+        src={smiley}
+        alt="Fancy display for superficial people"
+        className={styles.hero__image}
+        style={{ opacity: 0, position: 'absolute'}}
+      />
 
     </div>
   );
