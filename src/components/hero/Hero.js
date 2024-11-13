16b2538e461b3a70805e0620481f1fded46fe009
MTLY-1610 <larrouy.m@me.com>
2024-11-13 11:28:04 +0100

    quote svg and stuff

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -3,23 +3,29 @@ import gsap  from 'gsap';
 import styles from './Hero.module.css';
 import Image from 'next/image';
 import smiley from 'public/img/smiley.png';
+import quote from 'public/img/quote.svg'
 
 
 const Hero = () => {
   return (
     <section id='sectionZero' className={styles.hero__header}>
-      <p className={styles.p__one}><sup className={styles.quote}>,,</sup> My name is Mathieu but people call me Matt.
-      I am a french UI designer and developer. 
-      </p> 
-      <p className={styles.p__one}>I like make   cool and user-centric interfaces.   <Image
-              src={smiley}
-              alt="Fancy display for superficial people"
-              className={styles.hero__image}
-         
-            />
-   
-      </p>
-     
+      <div
+      <div className={styles.quote__wrapper}>
+        <Image
+               src={quote}
+               alt="Fancy display for superficial people"
+               className={styles.quote}
+        />
+      </div>
+      <div className={styles.text__wrapper}>
+        <p > My name is Mathieu but people call me Matt.<br/>
+        I am a french UI designer and developer. <br/>
+        I like to make   cool and user-centric interfaces.   <Image
+                src={smiley}
+                alt="Fancy display for superficial people"
+                className={styles.hero__image}/>
+        </p>
+      </div>
    </section>
   );
 };
